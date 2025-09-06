42bc7d4f5a7eec091ba488dd5eb8a8fb7b0d0bbb
julianfernandez24 <julianfernandez197612@gmail.com>
2025-09-06 10:54:24 -0300

    Add files via upload

---
+++ trabajo practico de estrutur de datos/trbajo.py
@@ -0,0 +1,52 @@
+#Clase Carpeta
+class Carpeta:
+    def __init__(self, nombre):
+        self.__nombre = nombre
+        self.__mensaje = []
+        
+def nombre(self):
+    return self.__nombre
+
+def agregar_mensaje(self, mensaje:
+    Mensaje):
+    self.__mensaje.append(mensaje)
+    
+    
+def listar_mensajes(self):
+    return self.__mensajes
+def __str__(self):
+    return f"Carpeta: {self.__nombr}
+({len(self.__mensajes)} mensajes)"
+        
+    
+
+
+
+
+
+
+
+
+#Clase Usuario
+class usuario:
+    def __init__(self,nombre, email):
+        self.__nombre = nombre
+        self.__email = email
+        self.__carpetas = {"Entradas"
+Carpeta("Entrada"), "Enviados":
+Carpeta("Entrada")}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
